426e985b3f7a9b099db5ae0ec576a3708673270a
flw <flw@cpan.org>
2018-06-26 15:58:56 +0800

    Feat: add Fortune as default handler

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"math/rand"
 	"os"
 	"strings"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/flw-cn/go-fortune"
 	"github.com/flw-cn/go-slackbot"
-	"github.com/flw-cn/go-smart-config"
+	"github.com/flw-cn/go-smartConfig"
 	"github.com/flw-cn/slack"
 	"github.com/pityonline/china-unix-slack-bot/service"
 	log "github.com/sirupsen/logrus"
@@ -52,6 +54,7 @@ func main() {
 	toMe.Hear("(?i)(hi|hello).*").MessageHandler(Hello)
 	toMe.Hear("(?i)(ping).*").MessageHandler(Ping)
 	toMe.Hear("(?i)(ip) .*").MessageHandler(QueryIP)
+	toMe.MessageHandler(Fortune)
 
 	bot.Run(true, nil)
 }
@@ -81,5 +84,40 @@ func QueryIP(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
 		ip := parts[1]
 		m = service.IPQuery(api, ip)
 	}
+	bot.Reply(evt, m, slackbot.WithTyping)
+}
+
+func Fortune(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
+	var err error
+
+	r := rand.Intn(100)
+	m := "听不懂你在说什么，不如我给你念首诗吧！"
+	o := ""
+	if r < 10 {
+		m = m + "\n" + "你想听唐诗还是宋词？"
+	} else if r > 95 {
+		m = m + "\n" + "中国话太难背了，咱还是说母语吧！"
+		o, err = fortune.Draw(
+			fortune.Category("literature", 40),
+			fortune.Category("riddles", 30),
+			fortune.Category("fortunes", 30),
+		)
+		if err != nil {
+			log.Printf("error: %#v", err)
+		}
+	} else {
+		o, err = fortune.Draw(
+			fortune.Category("tang300", 50),
+			fortune.Category("song100", 50),
+		)
+		if err != nil {
+			log.Printf("error: %#v", err)
+		}
+	}
+
+	if o != "" {
+		m = m + "\n" + o
+	}
+
 	bot.Reply(evt, m, slackbot.WithTyping)
 }
