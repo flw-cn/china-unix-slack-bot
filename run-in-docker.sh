cc0e355c4b192ec53c27f50448cd57fe4e94ddda
flw <flw@cpan.org>
2018-07-19 18:45:34 +0800

    Feat: update run-in-docker.sh

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "$SLACK_TOKEN" ]; then
     echo Must set environment variable SLACK_TOKEN first.
@@ -7,6 +7,11 @@ fi
 
 if [ -z "$TULING_API_KEY" ]; then
     echo Must set environment variable TULING_API_KEY first.
+    exit
+fi
+
+if [ -z "$RUYI_APP_KEY" ]; then
+    echo Must set environment variable RUYI_APP_KEY first.
     exit
 fi
 
@@ -25,6 +30,7 @@ docker run --name slack-bot $DOCKER_ARGS    \
     -v $PLAYGROUND:/slack-bot/playground    \
     -v $DOCKER_SOCK:$DOCKER_SOCK            \
     --env TULING_API_KEY=$TULING_API_KEY    \
-    slack-bot $BOT_ARGS                     \
-    --token $SLACK_TOKEN                    \
-    --play.host $PLAYGROUND
+    flwos/slack-bot $BOT_ARGS               \
+    --frontend.slack.token $SLACK_TOKEN     \
+    --backend.tuling.token $TULING_API_KEY  \
+    --backend.ruyi.appkey $RUYI_APP_KEY
